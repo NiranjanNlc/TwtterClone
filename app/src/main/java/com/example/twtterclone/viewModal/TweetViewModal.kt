b7e27431667a89e1c6572fafd1d8f1150a86a382
niranjannlc <niranjannlc10@gmail.com>
2021-12-27 20:11:02 +1200

    introducng the tweet vewmodel

--- app/src/main/java/com/example/twtterclone/viewModal/TweetViewModal.kt
+++ app/src/main/java/com/example/twtterclone/viewModal/TweetViewModal.kt
@@ -1,5 +1,27 @@
 package com.example.twtterclone.viewModal
 
-class TweetViewModal
+import androidx.lifecycle.ViewModel
+import com.example.twtterclone.model.data.Tweet
+import com.example.twtterclone.model.repo.TweetRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class TweetViewModal (private val repository: TweetRepo) : ViewModel()
 {
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+    init
+    {
+
+    }
+
+    fun insertTweet(tweet: Tweet)
+    {
+        coroutineScope.launch {
+            repository.insert(tweet)
+        }
+    }
+
 }
