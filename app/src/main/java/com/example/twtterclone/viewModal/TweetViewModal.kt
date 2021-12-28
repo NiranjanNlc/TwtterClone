e384a16022ba6006942131a9886f65d67a01bdff
niranjannlc <niranjannlc10@gmail.com>
2021-12-28 18:40:08 +1200

    data binding in compose tweet and view model

--- app/src/main/java/com/example/twtterclone/viewModal/TweetViewModal.kt
+++ app/src/main/java/com/example/twtterclone/viewModal/TweetViewModal.kt
@@ -3,13 +3,12 @@ package com.example.twtterclone.viewModal
 import androidx.lifecycle.ViewModel
 import com.example.twtterclone.model.data.Tweet
 import com.example.twtterclone.model.repo.TweetRepo
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import com.example.twtterclone.util.SampleTweet
+import kotlinx.coroutines.*
 
 class TweetViewModal (private val repository: TweetRepo) : ViewModel()
 {
+    var tweetmessage = String()
     private val parentJob = Job()
     private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
     init
@@ -17,11 +16,13 @@ class TweetViewModal (private val repository: TweetRepo) : ViewModel()
 
     }
 
-    fun insertTweet(tweet: Tweet)
+    fun insertTweet()
     {
-        coroutineScope.launch {
+        val tweet = SampleTweet.getTweet(tweetmessage)
+        runBlocking{
             repository.insert(tweet)
         }
+        tweetmessage = ""
     }
 
 }
