38a82e9d4c8e050dfc6f4c541b56f59e10f1f33c
niranjannlc <niranjannlc10@gmail.com>
2022-01-03 22:58:11 +1200

    fragment refactored

--- app/src/main/java/com/example/twtterclone/view/Authenciation.kt
+++ app/src/main/java/com/example/twtterclone/view/Authenciation.kt
@@ -2,6 +2,7 @@ package com.example.twtterclone.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.twtterclone.R
 import com.example.twtterclone.view.fragemnt.SighnUpFragment
 
 class Authenciation : AppCompatActivity() {
@@ -9,10 +10,10 @@ class Authenciation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.authenciation_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, SighnUpFragment.newInstance())
-                .commitNow()
-        }
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, SighnUpFragment.newInstance())
+//                .commitNow()
+//        }
     }
 }
--- app/src/main/java/com/example/twtterclone/view/fragemnt/SighnUpFragment.kt
+++ app/src/main/java/com/example/twtterclone/view/fragemnt/SighnUpFragment.kt
@@ -1,33 +1,22 @@
 package com.example.twtterclone.view.fragemnt
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.twtterclone.R
-import com.example.twtterclone.viewModal.AuthenciationViewModel
 
 class SighnUpFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = SighnUpFragment()
-    }
-
-    private lateinit var viewModel: AuthenciationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View
+    {
         return inflater.inflate(R.layout.sighn_up_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AuthenciationViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
