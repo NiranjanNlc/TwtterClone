08d00a3b1fe9d75d365e22457e11264b92bde909
niranjannlc <niranjannlc10@gmail.com>
2022-01-04 22:59:36 +1200

    introduvting fragment for auhenciation

--- app/src/main/java/com/example/twtterclone/view/Authenciation.kt
+++ app/src/main/java/com/example/twtterclone/view/Authenciation.kt
@@ -1,15 +1,28 @@
 package com.example.twtterclone.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.example.twtterclone.R
-import com.example.twtterclone.view.fragemnt.SighnUpFragment
+import com.example.twtterclone.view.fragemnt.IntroductoryFragment
+
 
 class Authenciation : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.authenciation_activity)
-
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<IntroductoryFragment>(R.id.container)
+            }
+        }
     }
+
+
+
 }
