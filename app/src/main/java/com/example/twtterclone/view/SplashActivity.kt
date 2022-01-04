aa5cf58fa426083ca6555889b38298ba49431765
niranjannlc <niranjannlc10@gmail.com>
2022-01-04 22:23:38 +1200

    splash activity for splash scree

--- app/src/main/java/com/example/twtterclone/view/SplashActivity.kt
+++ app/src/main/java/com/example/twtterclone/view/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.example.twtterclone.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 
 class SplashActivity : AppCompatActivity()
@@ -11,8 +12,11 @@ class SplashActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        if (isUserLoggedIn())
+        val authStautsu = isUserLoggedIn()
+        Log.i(" splash activity ", authStautsu.toString())
+        if (authStautsu)
         {
+            println()
             startHomeActivity()
         }
         else
@@ -23,14 +27,15 @@ class SplashActivity : AppCompatActivity()
 
     private fun isUserLoggedIn(): Boolean
     {
-       return true
+       return false
     }
     private fun startHomeActivity() {
         val intent = Intent(this, TimeLineActivity::class.java)
         startActivity(intent)
         Toast.makeText(applicationContext, "your icon was clicked", Toast.LENGTH_SHORT)
             .show()
-    }private fun startAuthActivity() {
+    }
+    private fun startAuthActivity() {
     val intent = Intent(this, Authenciation::class.java)
     startActivity(intent)
     Toast.makeText(applicationContext, "your icon was clicked", Toast.LENGTH_SHORT)
