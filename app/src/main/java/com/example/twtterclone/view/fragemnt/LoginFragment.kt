c6cf6a4b71ce1f1ffdcb5abe1cc9b2692d06abc0
niranjannlc <niranjannlc10@gmail.com>
2022-01-05 00:00:20 +1200

    completing the flow of authenciation in navigation

--- app/src/main/java/com/example/twtterclone/view/fragemnt/LoginFragment.kt
+++ app/src/main/java/com/example/twtterclone/view/fragemnt/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.twtterclone.R
 import com.example.twtterclone.databinding.FragmentIntroductoryBinding
 import com.example.twtterclone.databinding.FragmentLoginBinding
@@ -20,6 +21,13 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(layoutInflater,container,false)
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvSignUp.setOnClickListener{
+            it.findNavController().navigate(R.id.action_loginFragment_to_sighnUpFragment)
+        }
     }
 
 
