bb59368b1832fe63f7f688feac8d160331c7ad61
niranjannlc <niranjannlc10@gmail.com>
2022-01-05 18:49:31 +1200

    prepared the skeleton of the login

--- app/src/main/java/com/example/twtterclone/view/fragemnt/LoginFragment.kt
+++ app/src/main/java/com/example/twtterclone/view/fragemnt/LoginFragment.kt
@@ -1,15 +1,16 @@
 package com.example.twtterclone.view.fragemnt
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.example.twtterclone.R
-import com.example.twtterclone.databinding.FragmentIntroductoryBinding
 import com.example.twtterclone.databinding.FragmentLoginBinding
-import com.example.twtterclone.databinding.SighnUpFragmentBinding
+import com.example.twtterclone.view.TimeLineActivity
 
 class LoginFragment : Fragment() {
 
@@ -28,8 +29,33 @@ class LoginFragment : Fragment() {
         binding.tvSignUp.setOnClickListener{
             it.findNavController().navigate(R.id.action_loginFragment_to_sighnUpFragment)
         }
+        binding.btnLogin.setOnClickListener {
+            if (checkLoginScenario())
+            {
+                showTimeLine()
+            }
+            else
+            {
+              displayLoginErrorMessage()
+            }
+        }
     }
 
+    private fun showTimeLine() {
+        val intent = Intent(activity, TimeLineActivity::class.java)
+        startActivity(intent)
+        Toast.makeText(context, "your icon was clicked", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun displayLoginErrorMessage() {
+        TODO("Not yet implemented")
+    }
+
+    private fun checkLoginScenario(): Boolean {
+         return true
+
+    }
 
 
 }
